c2b3601be1c08f47c5fc92d3dcadfff177c75aae
JACJava <juliejava@icloud.com>
2019-02-16 14:42:16 -0600

    Added status messages and updated pom.xml with the plugin to create an executable .jar file

--- src/main/java/com/pluralsight/testing/m2/Main.java
+++ src/main/java/com/pluralsight/testing/m2/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println();
+        System.out.println("This can now be run from a jar file that was built using Maven!!!");
+        System.out.println();
+
     }
 }
